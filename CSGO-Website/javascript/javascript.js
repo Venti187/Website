357b82bfb9fcab924a9a890a7d0040fcd17ea273
Venti187 <131137367+Venti187@users.noreply.github.com>
2023-05-23 07:56:01 +0200

    Updated Weapons

--- CSGO-Website/javascript/javascript.js
+++ CSGO-Website/javascript/javascript.js
@@ -16,3 +16,16 @@ window.onload = function(){
   }
 
 }
+
+function toggleTable() {
+  var table = document.getElementById("myTable");
+  var span = document.querySelector("#assault-rifles .rifles-pictures span");
+  
+  if (table.style.display === "none") {
+    table.style.display = "table";
+    span.style.display = "none";
+  } else {
+    table.style.display = "none";
+    span.style.display = "block";
+  }
+}
